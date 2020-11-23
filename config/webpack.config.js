fab6e68c85cb22ed7cf0c737d01f7225fc1eb898
Tomasz Wroński <tomashh11@gmail.com>
2020-11-23 11:13:48 +0100

    ver 1.0

---
+++ config/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/index.js',
+    },
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, '../', 'build')
+    },
+    devServer: {
+        open: true,
+        contentBase: path.resolve(__dirname, '../', 'public'),
+        port: 3000
+    },
+    module: {
+        rules: [{
+                test: /\.txt$/,
+                use: 'raw-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(jpg|png|jpeg)$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        ["@babel/preset-env", {
+                            useBuiltIns: 'usage',
+                            corejs: "2.0.0"
+                        }]
+                    ],
+                    plugins: [
+                        "@babel/plugin-proposal-class-properties"
+                    ]
+                }
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+        }),
+    ]
+}
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/js/bootstrap';
+import './sass/style.scss';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+import {
+    library,
+    dom
+} from '@fortawesome/fontawesome-svg-core'
+import {
+    fas
+} from '@fortawesome/free-solid-svg-icons'
+import {
+    far
+} from '@fortawesome/free-regular-svg-icons'
+import {
+    fab
+} from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
+
+dom.i2svg()
