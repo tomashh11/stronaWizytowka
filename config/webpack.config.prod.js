e872a68fe4e539feddfa854bd570cb877512964e
Tomasz Wroński <tomashh11@gmail.com>
2021-01-08 22:41:15 +0100

    ver 1.2. - remove scroller, fix breakpoints and add info about readme file

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -67,23 +67,7 @@ module.exports = {
                     }
                 ]
 
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: [
-                        ["@babel/preset-env", {
-                            useBuiltIns: 'usage',
-                            corejs: "2.0.0"
-                        }]
-                    ],
-                    plugins: [
-                        "@babel/plugin-proposal-class-properties"
-                    ]
-                }
-            },
+            }
         ]
     },
     plugins: [
--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import 'bootstrap/dist/js/bootstrap';
 import './sass/style.scss';
-import Scroller from './js/Scroller';
-
-document.addEventListener('DOMContentLoaded', function(){
-    const scroller = new Scroller();
-});
